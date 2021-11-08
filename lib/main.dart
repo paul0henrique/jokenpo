f28e71fa7cfc1eee1837ac4c889a9733f995a99d
Paulo Henrique <paulohenriquesms10@gmail.com>
2021-11-08 13:00:16 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'Jogo.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Jokenpo",
+    home: Jogo(),
+  ));
+}
